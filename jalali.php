5c56cf247893987cf621ba88b8b75562a0fab1c1
MMD <natilosnts@gmail.com>
2024-12-14 23:18:49 +0330

    Update jalali.php

--- jalali.php
+++ jalali.php
@@ -3,11 +3,11 @@
 class time
 {
     private static $jalaliMonths = [
-        'فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند'
+        'فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند',
     ];
 
     private static $jalaliDays = [
-        'شنبه', 'یک‌شنبه', 'دوشنبه', 'سه‌شنبه', 'چهارشنبه', 'پنج‌شنبه', 'جمعه'
+        'شنبه', 'یک‌شنبه', 'دوشنبه', 'سه‌شنبه', 'چهارشنبه', 'پنج‌شنبه', 'جمعه',
     ];
 
     private static $timezoneOffset = 0; // جابجایی منطقه زمانی بر حسب ساعت
@@ -22,20 +22,22 @@ class time
     public static function toj($timestamp)
     {
         $timestamp += self::$timezoneOffset; // اعمال جابجایی منطقه زمانی
-        list($gYear, $gMonth, $gDay) = explode('-', date('Y-m-d', $timestamp));
-        list($jYear, $jMonth, $jDay) = self::gregorianToJalali($gYear, $gMonth, $gDay);
-        $time = date('H:i:s', $timestamp);
+        [$gYear, $gMonth, $gDay] = explode('-', date('Y-m-d', $timestamp));
+        [$jYear, $jMonth, $jDay] = self::gregorianToJalali($gYear, $gMonth, $gDay);
+        $time                    = date('H:i:s', $timestamp);
+
         return new self(sprintf('%04d/%02d/%02d %s', $jYear, $jMonth, $jDay, $time));
     }
 
     // تابعی برای تبدیل تاریخ جلالی به تایم‌استمپ
     public static function tot($jalaliDate, $format = 'Y/m/d H:i:s')
     {
-        list($datePart, $timePart) = explode(' ', $jalaliDate) + [1 => '00:00:00'];
-        list($jYear, $jMonth, $jDay) = explode('/', $datePart);
-        list($gYear, $gMonth, $gDay) = self::jalaliToGregorian($jYear, $jMonth, $jDay);
-        $gregorianDate = sprintf('%04d-%02d-%02d %s', $gYear, $gMonth, $gDay, $timePart);
-        $timestamp = strtotime($gregorianDate);
+        [$datePart, $timePart]   = explode(' ', $jalaliDate) + [1 => '00:00:00'];
+        [$jYear, $jMonth, $jDay] = explode('/', $datePart);
+        [$gYear, $gMonth, $gDay] = self::jalaliToGregorian($jYear, $jMonth, $jDay);
+        $gregorianDate           = sprintf('%04d-%02d-%02d %s', $gYear, $gMonth, $gDay, $timePart);
+        $timestamp               = strtotime($gregorianDate);
+
         return $timestamp - self::$timezoneOffset; // اعمال جابجایی معکوس منطقه زمانی
     }
 
@@ -51,6 +53,7 @@ class time
         $timestamp = self::tot($this->jalaliDateTime);
         $timestamp += $hours * 3600;
         $this->jalaliDateTime = self::toj($timestamp)->jalaliDateTime;
+
         return $this;
     }
 
@@ -59,13 +62,14 @@ class time
         $timestamp = self::tot($this->jalaliDateTime);
         $timestamp += $days * 86400;
         $this->jalaliDateTime = self::toj($timestamp)->jalaliDateTime;
+
         return $this;
     }
 
     public function addM($months)
     {
-        list($datePart, $timePart) = explode(' ', $this->jalaliDateTime);
-        list($jYear, $jMonth, $jDay) = explode('/', $datePart);
+        [$datePart, $timePart]   = explode(' ', $this->jalaliDateTime);
+        [$jYear, $jMonth, $jDay] = explode('/', $datePart);
         $jMonth += $months;
 
         while ($jMonth > 12) {
@@ -78,16 +82,18 @@ class time
         }
 
         $this->jalaliDateTime = sprintf('%04d/%02d/%02d %s', $jYear, $jMonth, $jDay, $timePart);
+
         return $this;
     }
 
     public function addY($years)
     {
-        list($datePart, $timePart) = explode(' ', $this->jalaliDateTime);
-        list($jYear, $jMonth, $jDay) = explode('/', $datePart);
+        [$datePart, $timePart]   = explode(' ', $this->jalaliDateTime);
+        [$jYear, $jMonth, $jDay] = explode('/', $datePart);
         $jYear += $years;
 
         $this->jalaliDateTime = sprintf('%04d/%02d/%02d %s', $jYear, $jMonth, $jDay, $timePart);
+
         return $this;
     }
 
@@ -107,9 +113,9 @@ class time
         $gm = $gMonth - 1;
         $gd = $gDay - 1;
 
-        $gDayNo = 365 * $gy + (int)(($gy + 3) / 4) - (int)(($gy + 99) / 100) + (int)(($gy + 399) / 400);
+        $gDayNo = 365 * $gy + (int) (($gy + 3) / 4) - (int) (($gy + 99) / 100) + (int) (($gy + 399) / 400);
 
-        for ($i = 0; $i < $gm; ++$i) {
+        for ($i = 0; $i < $gm; $i++) {
             $gDayNo += $gDaysInMonth[$i];
         }
 
@@ -121,24 +127,24 @@ class time
 
         $jDayNo = $gDayNo - 79;
 
-        $jNp = (int)($jDayNo / 12053);
+        $jNp = (int) ($jDayNo / 12053);
         $jDayNo %= 12053;
 
-        $jYear = 979 + 33 * $jNp + 4 * (int)($jDayNo / 1461);
+        $jYear = 979 + 33 * $jNp + 4 * (int) ($jDayNo / 1461);
 
         $jDayNo %= 1461;
 
         if ($jDayNo >= 366) {
-            $jYear += (int)(($jDayNo - 1) / 365);
+            $jYear += (int) (($jDayNo - 1) / 365);
             $jDayNo = ($jDayNo - 1) % 365;
         }
 
-        for ($i = 0; $i < 11 && $jDayNo >= $jDaysInMonth[$i]; ++$i) {
+        for ($i = 0; $i < 11 && $jDayNo >= $jDaysInMonth[$i]; $i++) {
             $jDayNo -= $jDaysInMonth[$i];
         }
 
         $jMonth = $i + 1;
-        $jDay = $jDayNo + 1;
+        $jDay   = $jDayNo + 1;
 
         return [$jYear, $jMonth, $jDay];
     }
@@ -153,9 +159,9 @@ class time
         $jMonth--;
         $jDay--;
 
-        $jDayNo = 365 * $jYear + (int)($jYear / 33) * 8 + (int)(($jYear % 33 + 3) / 4);
+        $jDayNo = 365 * $jYear + (int) ($jYear / 33) * 8 + (int) (($jYear % 33 + 3) / 4);
 
-        for ($i = 0; $i < $jMonth; ++$i) {
+        for ($i = 0; $i < $jMonth; $i++) {
             $jDayNo += $jDaysInMonth[$i];
         }
 
@@ -163,14 +169,14 @@ class time
 
         $gDayNo = $jDayNo + 79;
 
-        $gy = 1600 + 400 * (int)($gDayNo / 146097);
+        $gy = 1600 + 400 * (int) ($gDayNo / 146097);
         $gDayNo %= 146097;
 
         $leap = true;
 
         if ($gDayNo >= 36525) {
             $gDayNo--;
-            $gy += 100 * (int)($gDayNo / 36524);
+            $gy += 100 * (int) ($gDayNo / 36524);
             $gDayNo %= 36524;
 
             if ($gDayNo >= 365) {
@@ -180,13 +186,13 @@ class time
             }
         }
 
-        $gy += 4 * (int)($gDayNo / 1461);
+        $gy += 4 * (int) ($gDayNo / 1461);
         $gDayNo %= 1461;
 
         if ($gDayNo >= 366) {
             $leap = false;
             $gDayNo--;
-            $gy += (int)($gDayNo / 365);
+            $gy += (int) ($gDayNo / 365);
             $gDayNo %= 365;
         }
 
@@ -195,7 +201,7 @@ class time
         }
 
         $gMonth = $i + 1;
-        $gDay = $gDayNo + 1;
+        $gDay   = $gDayNo + 1;
 
         return [$gy, $gMonth, $gDay];
     }
@@ -204,8 +210,8 @@ class time
     public static function format($timestamp, $format = 'Y/m/d H:i:s')
     {
         $timestamp += self::$timezoneOffset; // اعمال جابجایی منطقه زمانی
-        list($gYear, $gMonth, $gDay) = explode('-', date('Y-m-d', $timestamp));
-        list($jYear, $jMonth, $jDay) = self::gregorianToJalali($gYear, $gMonth, $gDay);
+        [$gYear, $gMonth, $gDay] = explode('-', date('Y-m-d', $timestamp));
+        [$jYear, $jMonth, $jDay] = self::gregorianToJalali($gYear, $gMonth, $gDay);
 
         $formattedDate = $format;
         $formattedDate = str_replace('Y', $jYear, $formattedDate);
@@ -213,7 +219,7 @@ class time
         $formattedDate = str_replace('d', sprintf('%02d', $jDay), $formattedDate);
         $formattedDate = str_replace('F', self::$jalaliMonths[$jMonth - 1], $formattedDate);
         $formattedDate = str_replace('l', self::$jalaliDays[date('w', $timestamp)], $formattedDate);
-        $formattedDate .= ' ' . date('H:i:s', $timestamp);
+        $formattedDate .= ' '.date('H:i:s', $timestamp);
 
         return $formattedDate;
     }
@@ -222,6 +228,7 @@ class time
     public static function miladi($jalaliDateTime)
     {
         $timestamp = self::tot($jalaliDateTime);
+
         return date('Y-m-d H:i:s', $timestamp);
     }
 }
@@ -231,7 +238,7 @@ class time
 time::Timezone(3.5);
 
 // تبدیل تایم‌استمپ به تاریخ جلالی
-$timestamp = time();
+$timestamp  = time();
 $jalaliDate = time::toj($timestamp);
 echo "Jalali Date: {$jalaliDate}\n";
 
@@ -243,7 +250,7 @@ echo "Modified Jalali Date: {$modifiedDate}\n";
 echo time::format($timestamp, 'l d F Y H:i'); // نمایش روز، ماه و سال به فارسی
 
 // تبدیل تاریخ جلالی به تایم‌استمپ
-$jalaliDateInput = '1402/09/24 14:30:00';
+$jalaliDateInput    = '1402/09/24 14:30:00';
 $convertedTimestamp = time::tot($jalaliDateInput);
 echo "Timestamp: $convertedTimestamp\n";
 
